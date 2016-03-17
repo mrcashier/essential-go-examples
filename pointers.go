1646f7e30afb022cd4882537ff9be9d997371666
Carlos Cajero <ccajero@gmail.com>
2016-03-16 18:34:52 -0600

    swap with pointers

--- pointers.go
+++ pointers.go
@@ -9,6 +9,13 @@ func main() {
 
 	triple(&num)
 	fmt.Println(num)
+
+	a, b := 10, 20
+	fmt.Println("value of a and b", a, b)
+
+	swap(&a, &b)
+
+	fmt.Println("value of a and b", a, b)
 }
 
 func double(num int)  {
@@ -17,4 +24,10 @@ func double(num int)  {
 
 func triple(num *int) {
 	*num *= 3
+}
+
+func swap(x, y *int) {
+	t := *x
+	*x = *y
+	*y = t
 }
